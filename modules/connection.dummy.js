31daa436922bf9b893b33e2b19fcb35fa24a238d
Christian Caltabiano <ccaltabiano@obeliskdigital.com.au>
2020-05-01 10:25:37 +1000

    Create connection.dummy.js

---
+++ modules/connection.dummy.js
@@ -0,0 +1,13 @@
+//real file stored in db folder
+
+const connecDb = {
+    host: 'localhost',
+    port: 3306,
+    user: 'username',
+    database: 'database',
+    password: 'password'
+}
+
+//connection.connect()
+
+module.exports = connecDb
